6bc73897823939881a164f8a58add34bad40a1db
nakibhasan2711 <naimhasan2711@diu.edu.bd>
2021-07-26 11:26:01 +0600

    rendering list on 26.07.21 2:58am

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {};
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
   render() {
-    return <h1>Hello World!</h1>;
+    return (
+      <div>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
