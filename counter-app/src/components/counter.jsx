0456a1cad643ea88934f25a0ae95e4fed08e4990
nakibhasan2711 <naimhasan2711@diu.edu.bd>
2021-07-30 20:51:39 +0600

    lifting the state up

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -5,26 +5,14 @@ class Counter extends Component {
   //     super();
   //     this.handleIncrement = this.handleIncrement.bind(this);
   //   }
-  state = {
-    value: this.props.counter.value,
-  };
-
-  handleIncrement = (product) => {
-    console.log(product);
-    this.setState({ value: this.state.value + 1 });
-  };
-
-  doHandleIncrement = () => {
-    this.handleIncrement({ id: 1 });
-  };
 
   render() {
-    console.log(this.props);
+    //console.log(this.props);
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.doHandleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -41,12 +29,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
